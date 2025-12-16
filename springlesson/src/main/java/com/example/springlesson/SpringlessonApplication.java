3f80ea462b13d03cbb49ef000cbd0da2e05e91c7
Umemoto <usu666918@gmail.com>
2025-12-16 11:09:16 +0900

    base.html（共通ヘッダー）の仮の書き換えとロゴ・アイコンの画像追加してます。

--- springlesson/src/main/java/com/example/springlesson/SpringlessonApplication.java
+++ springlesson/src/main/java/com/example/springlesson/SpringlessonApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringlessonApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringlessonApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(SpringlessonApplication.class, args);
+  }
 
 }
