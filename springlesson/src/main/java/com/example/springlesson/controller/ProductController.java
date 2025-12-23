791ff6d7ce672c481fff54a009e90cfd288c73cf
Oyama <hw18a025@oecu.jp>
2025-12-23 14:25:41 +0900

    商品詳細ページをコントローラーに追加しました。

--- springlesson/src/main/java/com/example/springlesson/controller/ProductController.java
+++ springlesson/src/main/java/com/example/springlesson/controller/ProductController.java
@@ -49,9 +49,75 @@ public class ProductController {
     return "product/product";
   }
   
-//商品詳細ページ（確認用）
+//商品詳細ページ（メロンパン）
   @GetMapping("/detail")
   public String productDetail() {
       return "product/product-melonpan";
+  }
+  
+//商品詳細ページ（ピーチタルト）
+  @GetMapping("/peachtart")
+  public String productPeach() {
+      return "product/product-peachtart";
+  }
+ 
+//商品詳細ページ（アニマルクッキー缶）
+  @GetMapping("/animalcookie")
+  public String productanimalcookie() {
+      return "product/product-animalcookie";
+  }
+
+//商品詳細ページ（アニバーサリーケーキ）
+  @GetMapping("/anniversarycake")
+  public String productanniversarycake() {
+      return "product/product-anniversarycake";
+  }
+  
+//商品詳細ページ（アップルパイ）
+  @GetMapping("/applepie")
+  public String productapplepie() {
+      return "product/product-applepie";
+  }
+
+//商品詳細ページ（焼きドーナツ）
+  @GetMapping("/bakeddonut")
+  public String productbakeddonut() {
+      return "product/product-bakeddonut";
+  }
+
+//商品詳細ページ（ココアエクレア）
+  @GetMapping("/cocoaeclair")
+  public String productcocoaeclair() {
+      return "product/product-cocoaeclair";
+  }
+ 
+//商品詳細ページ（チョコケーキ）
+  @GetMapping("/chocolatecake")
+  public String productchocolatecake() {
+      return "product/product-chocolatecake";
+  }
+ 
+//商品詳細ページ（ココナッツマカロン）
+  @GetMapping("/coconutmacaron")
+  public String productcoconutmacaron() {
+      return "product/product-coconutmacaron";
+  }
+
+//商品詳細ページ（クッキー缶）
+  @GetMapping("/cookie")
+  public String productcookie() {
+      return "product/product-cookie";
+  }
+ 
+//商品詳細ページ（ムーランルージュ）
+  @GetMapping("/eggfreerouge")
+  public String producteggfreerouge() {
+      return "product/product-eggfreerouge";
+  }
+ 
+//商品詳細ページ（レモンケーキ）
+  @GetMapping("/lemoncake")
+  public String productlemoncake() {
+      return "product/product-lemoncake";
   }
 }
