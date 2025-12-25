96bfd8dea2f04e61c35719d38c7cf0b4e3982a2f
kojima <workschool1963@gmail.com>
2025-12-25 16:29:47 +0900

    商品詳細画面、上部のカテゴリー表示

--- springlesson/src/main/java/com/example/springlesson/dto/ProductDTO.java
+++ springlesson/src/main/java/com/example/springlesson/dto/ProductDTO.java
@@ -1,6 +1,8 @@
 package com.example.springlesson.dto;
 
 import java.text.NumberFormat;
+import java.util.List;
+import java.util.ArrayList;
 
 import lombok.Data;
 
@@ -16,7 +18,24 @@ public class ProductDTO {
 
   private Long catId;
 
+  // 複数カテゴリー対応
+  private List<CategoryInfo> categories = new ArrayList<>();
+
   public String getFormattedPrice() {
     return NumberFormat.getNumberInstance().format(price);
+  }
+
+  // カテゴリー情報を保持する内部クラス
+  @Data
+  public static class CategoryInfo {
+    private Integer catId;
+    private String catName;
+
+    public CategoryInfo() {}
+
+    public CategoryInfo(Integer catId, String catName) {
+      this.catId = catId;
+      this.catName = catName;
+    }
   }
 }
